9a359030599f8932f8b93c0464493177a96ca0a3
aeschylus <scipioaffricanus@gmail.com>
2016-03-08 16:59:05 -0800

    changes that add missing props to graph, dependencies added, data re-built

--- data/buildDependencyTree.js
+++ data/buildDependencyTree.js
@@ -1,16 +1,15 @@
 var _ = require('underscore'),
-fs = require('fs'),
+    fs = require('fs'),
 
-elementsJSON = JSON.parse(fs.readFileSync('euclidElements.json', 'utf8', function(){ console.log('Beginning dependency tree calculation.'); }));
+    elementsJSON = JSON.parse(fs.readFileSync('euclidElements.json', 'utf8', function(){ console.log('Beginning dependency tree calculation.'); }));
 
 (function () {
   'use strict';
-  
+
   if ( !String.prototype.contains ) {
-        String.prototype.contains = function() {
-                  return String.prototype.indexOf.apply( this, arguments ) !== -1;
-                      
-        };
+    String.prototype.contains = function() {
+      return String.prototype.indexOf.apply( this, arguments ) !== -1;
+    };
   }
 
   var depsJSON = elementsJSON.map(function(book, index) {
@@ -18,9 +17,8 @@ elementsJSON = JSON.parse(fs.readFileSync('euclidElements.json', 'utf8', functio
     return book.propositions.map(function(prop) {
       return {
         book: bookNumber,
-        prop: prop.id, 
+        prop: prop.id,
         propDependencies: prop.deps.filter(function(dep) {
-          console.log(typeof dep);
           if (dep.contains('post') || dep.contains('def') || dep.contains('c')) {
             return false;
           }
@@ -35,29 +33,31 @@ elementsJSON = JSON.parse(fs.readFileSync('euclidElements.json', 'utf8', functio
       };
     });
   }),
-  mergedDeps = [],
-  mergedDeps = mergedDeps.concat.apply(mergedDeps, depsJSON),
-  nodeIndex = (function() {
-    var nodeIndex = {};
-    mergedDeps.forEach(function(node, index) {
-      nodeIndex[node.prop] = index;
-    });
-    console.log(nodeIndex);
-    return nodeIndex;
-  })(),
-  nodesAndLinks = {
-    nodes: mergedDeps,
-    links: mergedDeps.map(function(node) {
-      return node.propDependencies.map(function(dep, depIndex) {
-        return {
-          source: nodeIndex[node.prop],
-          target: nodeIndex[dep]
-        }
-      });
-    }).reduce(function(propAlinks, propBlinks) {
-      return propAlinks.concat(propBlinks);
-    })
-  };
+      mergedDeps = [],
+      mergedDeps = mergedDeps.concat.apply(mergedDeps, depsJSON),
+      nodeIndex = (function() {
+        var nodeIndex = {};
+        mergedDeps.forEach(function(node, index) {
+          nodeIndex[node.prop] = index;
+        });
+        console.log(nodeIndex);
+        return nodeIndex;
+      })(),
+      nodesAndLinks = {
+        nodes: mergedDeps,
+        links: mergedDeps.map(function(node) {
+          return node.propDependencies.map(function(dep, depIndex) {
+            console.log(nodeIndex[dep]);
+            console.log(nodeIndex[node.prop]);
+            return {
+              source: nodeIndex[node.prop],
+              target: nodeIndex[dep]
+            };
+          });
+        }).reduce(function(a, b) {
+          return a.concat(b);
+        })
+      };
 
   fs.writeFileSync('euclidElementsDeps.json', JSON.stringify(nodesAndLinks));
 
--- js/euclid.js
+++ js/euclid.js
@@ -1,80 +1,81 @@
-var depDirection = "down";
 $(function() {
 
-  var width = window.innerWidth, height = window.innerHeight;
-  // var width = 940,
-  //     height = 600;
+  var nodes = null;
+  var connections = null;
+
+  var width = window.innerWidth,
+      height = window.innerHeight;
 
   var svg = d3.select("#chart").append("svg")
-  .attr("width", width)
-  .attr("height", height)
+        .attr("width", width)
+        .attr("height", height);
+
+  var container = svg.append('g');
+
+  var fill = d3.scale.category10();
+  var zoom = d3.behavior.zoom()
+        .scaleExtent([0.2, 10])
+        .on("zoom", zoomed);
+
+  function zoomed() {
+    container.attr("transform",
+             "translate(" + zoom.translate() + ") " +
+             "scale(" + zoom.scale() + ")"
+            );
+    console.log('something is happening');
+  }
+
+  svg.call(zoom);
 
   var force = d3.layout.force()
-  .gravity(.03)
-  .charge(-230)
-  .distance(200)
-  .size([width, height]);
+        .gravity(0.03)
+        .charge(-230)
+        .distance(200)
+        .size([width, height]);
 
   d3.json("data/euclidElementsDeps.json", function(json) {
-    console.log(json);
+    nodes = json.nodes;
+    connections = json.links.filter(function(link, index){
+      return link.target;
+    });
+
+    console.log(nodes);
+    console.log(connections);
+
+    hairballLayout(nodes,connections);
+  });
+
+  function hairballLayout(nodes, connections) {
     force
-    .nodes(json.nodes)
-    .links(json.links)
-    .start();
+      .nodes(nodes)
+      .links(connections)
+      .start();
 
-    var node_drag = d3.behavior.drag()
-    .on("dragstart", dragstart)
-    .on("drag", dragmove)
-    .on("dragend", dragend);
+    var link = container.selectAll(".link")
+          .data(connections)
+          .enter().append("line")
+          .attr("class", "link");
 
-    function dragstart(d, i) {
-      force.stop() // stops the force auto positioning before you start dragging
-    }
+    var node = container.selectAll(".node")
+          .data(nodes)
+          .enter().append("g")
+          .attr("class", 'node')
+          .style("fill", function(d, i) { return fill(d.book); });
 
-    function dragmove(d, i) {
-      d.px += d3.event.dx;
-      d.py += d3.event.dy;
-      d.x += d3.event.dx;
-      d.y += d3.event.dy; 
-      tick(); // this is the key to make it work together with updating both px,py,x,y on d!
-    };
-
-    function dragend(d, i) {
-      tick();
-      force.resume();
-    };
-
-    function dragOn() {
-    };
-
-    var link = svg.selectAll(".link")
-    .data(json.links)
-    .enter().append("line")
-    .attr("class", "link")
-
-    var node = svg.selectAll(".node")
-    .data(json.nodes)
-    .enter().append("g")
-    .attr("class", "node")
-    .call(force.drag);
-
-    node.on("mouseover", fade(.08)).on("mouseout", fade(1));
+    node.on("mouseover", fade(0.08)).on("mouseout", fade(1));
 
     node.append("circle")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("r", 7);
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("r", 7);
 
-    function clipTitle(prop) {
-      return prop.substr(prop.indexOf(".") + 1); 
-    }
-
-    var prop = function(d) { return clipTitle(d.prop) };
 
     node.append("text")
-    .attr("dx", 6)
-    .attr("dy", "-.3em")
-    .text(prop); 
+      .attr("dx", 6)
+      .attr("dy", "-.3em")
+      .text(function(d) {
+        return d.prop;
+      });
 
     var dependencies = function (d) {return d.propDependencies;};
 
@@ -82,21 +83,14 @@ $(function() {
 
     function tick() {
       link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
       node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
     }
 
     // Creates empty object literal into which we will place an assigned boolean value to each source-target pair.
     var linkedByIndex = {};
-
-    // Selects the links property of the d3 force layout json property,
-    // Which is an array, and runs the forEach built-in javascript array method on it, iterating over each.
-    json.links.forEach(function(d) {
-
-      linkedByIndex[d.source.index + "," + d.target.index] = 1;
-    });
 
     function references(a, b) {
       return linkedByIndex[a.index + "," + b.index] || b.index == a.index;
@@ -108,73 +102,87 @@ $(function() {
 
     function fade(opacity) {
       // Sets the opacity of the link and node elements based on the value of the "isConnected" function.
-      if ( depDirection === "up" ) {
-        console.log("was up, now:" + depDirection);
-        return function(d) {
-          node.style("stroke-opacity", function(o) {
-            // Ternary operator syntax in javascript.
-            // Checks the return value of the function.
-            // If true, thisOpacity will equal 1, if false, it will equal the 
-            // supplied opacity value, "o."
-            thisOpacity = referencedBy(d, o) ? 1 : opacity;
-            // "this" evaluates to the matched node, since this finction is
-            // called as a method on a jQuery or d3 matched set.
-            this.setAttribute('fill-opacity', thisOpacity);
-            return thisOpacity;
-          });
+      return function(d) {
+        node.style("stroke-opacity", function(o) {
+          // Ternary operator syntax in javascript.
+          // Checks the return value of the function.
+          // If true, thisOpacity will equal 1, if false, it will equal the
+          // supplied opacity value.
+          thisOpacity = references(d, o) ? 1 : opacity;
+          // "this" evaluates to the matched node, since this finction is
+          // called as a method on a jQuery or d3 matched set.
+          this.setAttribute('fill-opacity', thisOpacity);
+          return thisOpacity;
+        });
 
-          link.style("stroke-opacity", opacity).style("stroke-opacity", function(o) {
-            return o.target === d ? 1 : opacity;
-          });
-        };
-      } else {
-        console.log("was up, now:" + depDirection);
-        return function(d) {
-          node.style("stroke-opacity", function(o) {
-            // Ternary operator syntax in javascript.
-            // Checks the return value of the function.
-            // If true, thisOpacity will equal 1, if false, it will equal the 
-            // supplied opacity value.
-            thisOpacity = references(d, o) ? 1 : opacity;
-            // "this" evaluates to the matched node, since this finction is
-            // called as a method on a jQuery or d3 matched set.
-            this.setAttribute('fill-opacity', thisOpacity);
-            return thisOpacity;
-          });
-
-          link.style("stroke-opacity", opacity).style("stroke-opacity", function(o) {
-            return o.source === d ? 1 : opacity;
-          });
-        };
-      }
-
+        link.style("stroke-opacity", opacity).style("stroke-opacity", function(o) {
+          return o.source === d ? 1 : opacity;
+        });
+      };
     }
+  }
 
-    function showEnunciation() {
-      a = 2;
-    }
+  function columnLayout() {
+    force.stop();
 
-  });
+    nodes.forEach(function(node) {
+      node.x = (node.book * 250);
+      node.y = parseInt(node.prop.split('.')[2]) * 35;
+    });
+
+    container.selectAll('.node')
+      .data(nodes)
+      .transition()
+      .duration(2000)
+      .attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      });
+
+    container.selectAll('.node text')
+      .transition()
+      .duration(2000)
+      .attr("dx", -13)
+      .attr("dy", 6)
+      .attr("class", 'overview')
+      .text(function(d){
+        return d.prop.split('.')[2];
+      });
+
+    container.selectAll('.link')
+      .transition()
+      .duration(2000)
+      .attr("x1", function(d) {
+        console.log(d);
+        return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+    console.log('shifting');
+  }
+
+  key('right', function() { columnLayout(); });
+  key('left', function() { hairballLayout(nodes, connections); });
 
   $("#infoButton").toggle(
     function() {
-    $("#infoContainer").stop().fadeIn(800);
-    $("#chart").stop().fadeTo(400, 0.05);
-    $("#infoButton").addClass("selected");
-  }, function() {
-    $("#infoContainer").stop().fadeOut(400);
-    $("#chart").stop().fadeTo(400, 1);
-    $("#infoButton").removeClass("selected");
-  });
+      $("#infoContainer").stop().fadeIn(800);
+      $("#chart").stop().fadeTo(400, 0.05);
+      $("#infoButton").addClass("selected");
+    }, function() {
+      $("#infoContainer").stop().fadeOut(400);
+      $("#chart").stop().fadeTo(400, 1);
+      $("#infoButton").removeClass("selected");
+    });
   $("#setDirection").click(
     function () {
-    console.log(depDirection);
-    if ( depDirection == "up" ) { depDirection = "down";
-      $('.direction').html(depDirection+"2");
-    } 
-    else { depDirection = "up";}
-    $('.direction').html(depDirection);
-  });
+      console.log(depDirection);
+      if ( depDirection == "up" ) { depDirection = "down";
+                                    $('.direction').html(depDirection+"2");
+                                  }
+      else { depDirection = "up";}
+      $('.direction').html(depDirection);
+    });
 
   $('#infoContainer').click(function() {
     $("#infoButton").click();
@@ -184,15 +192,4 @@ $(function() {
     event.stopPropagation();
   });
 
-  var text = {
-    propNumber : 48,
-    dummyElement : "Derpety Derpety Derpety",
-    addProps : function() {
-      for (i=0; i<this.propNumber; i++) {
-        $("<div id='prop"+i+"'>"+this.dummyElement+"</div>").appendTo("body");
-      }
-    }
-  }
-
-  //text.addProps();
 });
